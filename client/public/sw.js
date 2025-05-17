2ee57ea7b1e7455f9889f9e7b23b49dca3ef1101
Jeffery Emuodafevware Onome <140074317+goldenjeffempire@users.noreply.github.com>
2025-05-17 07:09:27 +0100

    Implemented the Frontend

---
+++ client/public/sw.js
@@ -0,0 +1,24 @@
+
+const CACHE_NAME = 'echoverse-v1';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/assets/index.js',
+        '/assets/index.css'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
